5bed3095b662eab1638f27a3fba50484d9697fe7
BangDori <44726494+BangDori@users.noreply.github.com>
2024-01-22 12:32:24 +0900

    [Gold IV] Title: 합이 0, Time: 3212 ms, Memory: 111332 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.py
+++ 백준/Gold/3151. 합이 0/합이 0.py
@@ -1,44 +1,34 @@
+from bisect import bisect_left
 import sys
 input = sys.stdin.readline
-# 이분탐색
 
 N = int(input())
 people = list(map(int, input().split()))
 people.sort()
 
 answer = 0
+for i in range(N-2):
+    curr_people = people[i]
+    left, right = i+1, N-1
 
-def get_team_count(left, right, goal):
-    count = 0
-
-    max_idx = N
     while left < right:
-        tmp = people[left] + people[right]
+        team_result = people[left] + curr_people + people[right]
 
-        if tmp < goal:
-            left += 1
-
-        elif tmp == goal:
-            if people[left] == people[right]:
-                count += (right - left)
-            else:
-                if max_idx > right:
-                    max_idx = right
-                    while max_idx >= 0 and people[max_idx - 1] == people[right]:
-                        max_idx -= 1
-                count += right - max_idx + 1
-
-            left += 1
-
-        else:
+        # 0보다 크다면, 오른쪽을 이동시켜서 낮추기
+        if team_result > 0:
             right -= 1
-    
-    return count
 
-for i in range(N - 2):
-    start, end = i+1, N-1
-    goal = people[i] * -1
-
-    answer += get_team_count(start, end, goal)
+        # 0이하
+        else:
+            if team_result == 0:
+                
+                if people[left] == people[right]:
+                    answer += (right - left)
+                else:
+                    # people[right]와 동일한 인덱스 -> 이 떄 최대 인덱스를 구함
+                    pos = bisect_left(people, people[right])
+                    answer += (right - pos + 1)
+            
+            left += 1
 
 print(answer)
