5b681c813267c9e7d607ead1c2b4b7846e840b71
BangDori <44726494+BangDori@users.noreply.github.com>
2024-01-22 11:17:06 +0900

    [Silver III] Title: 다이나믹 롤러, Time: 1008 ms, Memory: 143280 KB -BaekjoonHub

--- 백준/Silver/17393. 다이나믹 롤러/다이나믹 롤러.py
+++ 백준/Silver/17393. 다이나믹 롤러/다이나믹 롤러.py
@@ -1,31 +1,13 @@
+from bisect import bisect_right
 import sys
 input = sys.stdin.readline
 
-def get_paint_pos(current_pos, current_ink):
-    left, right = current_pos+1, N-1
-    pos = current_pos
-
-    while left <= right:
-        mid = (left + right) // 2
-
-        if B[mid] <= current_ink:
-            left = mid + 1
-            pos = mid
-        elif current_ink < B[mid]:
-            right = mid - 1
-
-    return pos
-
 N = int(input())
-A = list(map(int, input().split())) # 잉크 지수
-B = list(map(int, input().split())) # 점도 지수
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
+answer = []
 for i in range(N):
     current_ink = A[i]
-    pos = get_paint_pos(i, A[i])
-
-    print(pos-i, end=' ')
-
-# 6
-# 1 2 3 4 5 6
-# 1 1 2 2 3 3
+    answer.append(bisect_right(B, current_ink) - (i+1))
+print(*answer)
