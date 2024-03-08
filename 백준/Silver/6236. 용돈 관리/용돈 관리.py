37acbd0e90759ce0e4430d996b3e4a5aeed536b4
BangDori <44726494+BangDori@users.noreply.github.com>
2024-03-08 12:17:50 +0900

    [Silver II] Title: 용돈 관리, Time: 200 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.py
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.py
@@ -1,44 +1,38 @@
 import sys
 input = sys.stdin.readline
 
-def getDay(withdraw_money, money_in_day):
-    tot_withdraw = 0
-    withdraw_count = 0 # 인출 횟수
-
+def get_day(withdraw_money, days):
+    tot_withdraw_count = 0 # 인출 횟수
     current_money = 0
-    for money in money_in_day:
+
+    for money in days:
         if money > current_money:
-            current_money = 0
-            tot_withdraw += 1
-            withdraw_count = money // withdraw_money if money % withdraw_money == 0 else (money // withdraw_money) + 1
-            current_money = (withdraw_count * withdraw_money)
+            tot_withdraw_count += 1            
+            current_money = withdraw_money
         
         current_money -= money
 
-    return tot_withdraw
+    return tot_withdraw_count
 
+def binary_search(days):
+    left = max(days) # 500
+    right = sum(days) # 1901
+    mid = (left + right) // 2 # 1200
 
-use_day, get_count = map(int, input().split())
-money_in_day = [int(input()) for _ in range(use_day)]
+    answer = 0
+    while left <= right:
+        mid = (left + right) // 2
+        tot_withdraw_count = get_day(mid, days)
 
-left = max(money_in_day)
-right = sum(money_in_day)
-mid = (left + right) // 2
+        if tot_withdraw_count == withdraw_count: answer = mid
 
-answer = 0
-while left <= right:
-    mid = (left + right) // 2
+        if tot_withdraw_count > withdraw_count: left = mid + 1
+        else: right = mid - 1
 
-    withdraw_count = getDay(mid, money_in_day)
+    return answer or mid
 
-    if withdraw_count == get_count: answer = mid
+day, withdraw_count = map(int, input().split())
+days = [int(input()) for _ in range(day)]
 
-    if withdraw_count > get_count:
-        left = mid + 1
-    else:
-        right = mid - 1
-
-if answer != 0:
-    print(answer)
-else:
-    print(mid)
+answer = binary_search(days)
+print(answer)
